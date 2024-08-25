d62e0455e8e702588ec1e936f890a10ed4e5e810
ajaybhalerao12 <ajaybhalerao12@gmail.com>
2024-08-25 19:24:10 +0530

    feat(controller): add POST, PUT and  DELETE methods for articles
    Implement the POST , PUT and DELETE method for the articles controller to handle creating, updating and deleting articles.

--- ArticlesPOSTGREDBCRUDOperations/Controllers/ArticlesController.cs
+++ ArticlesPOSTGREDBCRUDOperations/Controllers/ArticlesController.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 namespace ArticlesPOSTGREDBCRUDOperations.Controllers
 {
     [ApiController]
-    [Route("api/articles")]    
+    [Route("api/articles")]
     public class ArticlesController : ControllerBase
     {
         private readonly AppDBContext _context;
@@ -17,12 +17,79 @@ namespace ArticlesPOSTGREDBCRUDOperations.Controllers
             _context = context;
         }
 
-
         [HttpGet]
         // GET: api/articles
         public async Task<ActionResult<IEnumerable<Article>>> GetArticles()
         {
             return await _context.Articles.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        // GET: api/articles/{id}
+        public async Task<ActionResult<Article>> GetArticle(int id)
+        {
+            Article? article = await _context.Articles.FindAsync(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+            return article;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Article>> PostArticle(Article article)
+        {
+            article.CreatedAt = DateTime.UtcNow;
+            _context.Articles.Add(article);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetArticle", article.Id, article);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutArticle(int id, Article article)
+        {
+            if (id != article.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(article).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArticleExists(id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteArticle(int id)
+        {            
+            var article = await _context.Articles.FindAsync(id);
+
+            if (article == null)
+            {
+                return NotFound();
+            }
+            _context.Articles.Remove(article);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ArticleExists(int id)
+        {
+            return _context.Articles.Any(a => a.Id == id);
         }
     }
 }
