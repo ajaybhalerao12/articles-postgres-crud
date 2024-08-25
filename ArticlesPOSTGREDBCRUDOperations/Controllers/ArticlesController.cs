7c293883e0dbbbb96391051b7fcc8977f9923886
ajaybhalerao12 <ajaybhalerao12@gmail.com>
2024-08-25 18:53:53 +0530

    feat(controller): add articles controller and refactor code - Introduce new controller article fro reading the articles - Remove the weather controller and its depedent code

---
+++ ArticlesPOSTGREDBCRUDOperations/Controllers/ArticlesController.cs
@@ -0,0 +1,28 @@
+﻿using ArticlesPOSTGREDBCRUDOperations.Data;
+using ArticlesPOSTGREDBCRUDOperations.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArticlesPOSTGREDBCRUDOperations.Controllers
+{
+    [ApiController]
+    [Route("api/articles")]    
+    public class ArticlesController : ControllerBase
+    {
+        private readonly AppDBContext _context;
+
+        public ArticlesController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        // GET: api/articles
+        public async Task<ActionResult<IEnumerable<Article>>> GetArticles()
+        {
+            return await _context.Articles.ToListAsync();
+        }
+    }
+}
